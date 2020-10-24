c585069547c3dfae54c1a9bfeed9eaa6853e3e6e
Taiki Noda <air19990130@yahoo.co.jp>
2020-10-24 16:43:32 +0900

    javascripts

---
+++ app/assets/javascripts/tasks.js
@@ -0,0 +1,11 @@
+document.addEventListener('turbolonks:load', function() {
+  document.querySelectorAll('td').forEach(function(td) {
+    td.addEventListener('mouseover', function(e) {
+      e.currentTarget.style.backgroundColor = '#eff';
+    });
+
+    td.addEventListener('mouseout', function(e) {
+      e.currentTarget.style.backgroundColor = '';
+    });
+  });
+});
